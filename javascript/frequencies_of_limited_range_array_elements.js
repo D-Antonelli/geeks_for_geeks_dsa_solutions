d4e59873c82a79bfd720c4fa717edbfd877ecdc1
D-Antonelli <derya.antonelli@gmail.com>
2024-08-26 19:41:24 +0100

    frequency-of-array-elements

--- javascript/frequencies_of_limited_range_array_elements.js
+++ javascript/frequencies_of_limited_range_array_elements.js
@@ -1,1 +1,17 @@
 // https://www.geeksforgeeks.org/problems/frequency-of-array-elements-1587115620/1
+
+class Solution {
+  frequencyCount(arr, N, P) {
+    let res = new Array(N).fill(0);
+
+    for (let i = 0; i < N; i++) {
+      if (arr[i] - 1 < N) {
+        res[arr[i] - 1]++;
+      }
+    }
+
+    for (let i = 0; i < N; i++) {
+      arr[i] = res[i];
+    }
+  }
+}
